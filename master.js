1d0a9347b682a0d0039dd73019710775c88919e7
Bryan Reymonenq <bryan.reymonenq@gmail.com>
2015-06-29 19:17:53 +0200

    Amélioration du master et des serveurs de jeu

---
+++ master.js
@@ -0,0 +1,344 @@
+var XMLHttpRequest  = require("xmlhttprequest").XMLHttpRequest;
+var colors          = require( 'colors' );
+var shell           = require( 'shelljs' );
+var express 	    = require( 'express' );
+var path		    = require( 'path' );
+var mysql      	    = require( 'mysql' );
+var session 	    = require( 'express-session' );
+var bodyParser 	    = require( 'body-parser' );
+var app 		    = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'h4Q{3e;L',
+    database : 'ARDSweeper'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+/*
+Configurations
+*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: false } ));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "this is a secret passphrase"
+}));
+
+var MESSAGE = 0;
+var INFO = 1;
+var WARNING = 2;
+var ERROR = 3;
+var SUCCESS = 4;
+
+var port = 3000;
+
+var servers = [];
+var MAX_PLAYERS = 4;
+
+var error = {
+    status: "error",
+    code: 0,
+    message: ""
+};
+
+function setup() {
+    getServers(null);
+}
+
+function getServers(fn) {
+    var query = "SELECT * FROM servers";
+    connection.query(query, function(err, results) {
+        if(err)
+            return fn(new Error(err.code));
+        for(var i = 0; i < results.length; i++) {
+            servers.push(results[i]);
+            servers[i].live = false;
+        }
+    });
+}
+
+function getFreeServer() {
+    for(var i = 0; i < servers.length; i++) {
+        if(getPlayers(servers[i].id) < MAX_PLAYERS && servers[i].live)
+            return servers[i];
+    }
+    return null;
+}
+
+function getIndexServerByID( ID ) {
+    for(var i = 0; i < servers.length; i++) {
+        if(servers[i].id == ID)
+            return i;
+    }
+    return -1;
+}
+
+function watchServers() {
+    for(var i = 0; i < servers.length; i++) {
+        var now = new Date();
+        if(servers[i].live == false)
+            continue;
+        if(now - servers[i].last_ping > 15000) {
+            servers[i].live = false;
+            log(WARNING, "[SERVER][" + servers[i].id + "] SHUT DOWN...");
+        }
+    }
+}
+
+function getPlayers(serverID) {
+    var idx = getIndexServerByID(serverID);
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://localhost:" + servers[idx].port + "/players", false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+/*
+ EXPRESS
+ */
+function auth(username, password, fn) {
+    //MySQL Query
+    var query = "SELECT DISTINCT(users.id), users.username, tokens.token FROM users INNER JOIN tokens ON tokens.user_id = users.id WHERE users.username = '" + username + "' AND users.password = '" + password + "' ORDER BY tokens.created_at DESC";
+    connection.query(query, function(err, rows, fields) {
+        if(err)
+            return fn(new Error(err.code));
+        var user = {user: rows[0]};
+        if(!user.user) return fn(new Error("User does not exist"));
+        return fn(null, user);
+    });
+}
+
+function authToken(token, fn) {
+    //MySQL Query
+    var query = "SELECT DISTINCT(users.id), users.username, tokens.token FROM users INNER JOIN tokens ON tokens.user_id = users.id WHERE tokens.token = '" + token + "'";
+    connection.query(query, function(err, rows, fields) {
+        if(err)
+            return fn(new Error(err.code));
+        var user = {user: rows[0]};
+        if(!user.user) return fn(new Error("User does not exist"));
+        return fn(null, user);
+    });
+}
+
+function register(u, p, fn) {
+    var query = "INSERT INTO users SET ?";
+    if(!u || !p)
+        return fn(new Error("Les champs n'ont pas été renseignés."));
+
+    connection.query(query, {username: u, password: p}, function(err, rows, fields) {
+        if(err)
+            return fn(new Error("Erreur lors de l'inscription"));
+        var id = rows.insertId;
+        setToken(id, function(err, token) {
+            if(token)
+                return true;
+            return false;
+        })
+        if(!id) return fn(new Error("User not registered"));
+        return fn(null, id);
+    });
+}
+
+function getUserByID(ID, fn) {
+    connection.query("SELECT DISTINCT(users.id), users.username, tokens.token FROM users INNER JOIN tokens ON tokens.user_id = users.id WHERE users.id = ? ORDER BY tokens.created_at DESC", ID, function(err, rows, fields) {
+        if(err)
+            return fn(new Error(err.code));
+        var user = {user: rows[0]};
+        if(!user.user) return fn(new Error("User not found"));
+        return fn(null, user);
+    });
+}
+
+function setToken(ID, fn) {
+    var t = Math.floor((Math.random() * 99999999999999) + 1000000000000);
+    connection.query("INSERT INTO tokens SET ?", {user_id: ID, token: t}, function(err, rows, fields) {
+        if(err)
+            return fn(new Error("Token can't be created"));
+        token = rows.insertId;
+        if(!token) return fn(new Error("Token not found"));
+        return fn(null, token);
+    });
+}
+
+function _error(code, message) {
+    error.code = code;
+    error.message = message;
+    log(ERROR, message);
+}
+
+function log(type, message) {
+    var date = new Date();
+
+    //Parse date
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if(day < 10) day = "0" + day;
+    if(month < 10) month = "0" + month;
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
+
+    var output = day + "/" + month + "/" + date.getFullYear() + " - " + hours + ":" + minutes  + ":" + seconds + " :: " + message;
+
+    switch (type) {
+        case MESSAGE :
+            console.log(output);
+            break;
+
+        case SUCCESS :
+            console.log(colors.green(output));
+            break;
+
+        case INFO :
+            console.log(colors.cyan(output));
+            break;
+
+        case WARNING :
+            console.log(colors.yellow(output));
+            break;
+
+        case ERROR :
+            console.log(colors.red(output));
+            break;
+    }
+}
+
+
+/**
+ *	POST:
+ */
+
+app.post('/user/login', function(req, res) {
+    var username = req.body.username,
+        password = req.body.password;
+
+    log(INFO, "[USER][POST] Login request");
+
+    auth(username, password, function(err, user) {
+        if(user) {
+            req.session.regenerate(function() { req.session.user = user; });
+            log(SUCCESS, "[USER][POST] Login success for user : " + user.user.username);
+            user.server = getFreeServer();
+            user.server.portString = "" + user.server.port;
+            res.status(200).send(user);
+        } else {
+            _error(err.code, err.message);
+            res.status(404).send(error);
+        }
+    });
+});
+
+app.post('/user/login/:token', function(req, res) {
+    var token = req.params.token;
+
+    log(INFO, "[USER][POST] Login request by token");
+
+    authToken(token, function(err, user) {
+        if(user) {
+            req.session.regenerate(function() {
+                req.session.user = user;
+            });
+            log(SUCCESS, "[USER][POST] Login by token success for user : " + user.user.username);
+            user.server = getFreeServer();
+            user.server.portString = "" + user.server.port;
+            res.status(200).send(user);
+        } else {
+            _error(err.code, err.message);
+            res.status(404).send(error);
+        }
+    });
+});
+
+app.post('/user/register', function(req, res) {
+    var username = req.body.username,
+        password = req.body.password;
+
+    log(INFO, "[USER][POST] Register request");
+
+    register(username, password, function(err, ID) {
+        if(ID) {
+            getUserByID(ID, function(err2, user) {
+                if(user) {
+                    res.status(200).send(user);
+                    log(SUCCESS, "[USER][POST] Register success for User : " + user.user.username);
+                } else {
+                    _error(err2.code, err2.message);
+                    res.status(404).send(error);
+                }
+            });
+        } else {
+            _error(err.code, err.message);
+            res.status(404).send(error);
+        }
+    });
+});
+
+app.get('/', function(req, res) {
+   res.status(200).send("Hello World !");
+});
+
+app.get('/servers', function(req, res) {
+    //Liste des serveurs
+    log(INFO, "[SERVER][GET] get servers");
+    res.status(200).send(servers);
+});
+
+app.get('/server/:id/live', function(req, res) {
+    var idx = getIndexServerByID(req.params.id);
+    servers[idx].live = true;
+    servers[idx].last_ping = new Date();
+    log(MESSAGE, "[PING][" + servers[idx].id + "] serveur " + servers[idx].name + " is alive");
+    res.status(200).send(1);
+});
+
+app.get('/user/:id', function(req, res) {
+    log(INFO, "[USER][GET] user request");
+    var id = req.params.id;
+    getUserByID(id, function(err, user) {
+        if(user) {
+            log(SUCCESS, "[USER][GET] Get user success");
+            res.status(200).send(user);
+        } else {
+            _error(err.code, err.message);
+            res.status(404).send(error);
+        }
+    });
+});
+
+var server = http.listen( port, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    setup();
+    setInterval(watchServers, 1000);
+
+    shell.exec("clear");
+
+    console.log("################");
+    console.log("----------------");
+    console.log("\n");
+    console.log("## ARDSweeper Master Server");
+    log(SUCCESS, "map generated : OK");
+    log(SUCCESS, "servers loaded : OK");
+    log(SUCCESS, "servers pings : OK");
+    log("...");
+    log(SUCCESS, "Loading : OK");
+    log(INFO, "Master Server started at http://" + host + ":" + port);
+});
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-var express 	= require( 'express' );
-var path		= require( 'path' );
-var mysql      	= require( 'mysql' );
-var session 	= require( 'express-session' );
-var bodyParser 	= require( 'body-parser' );
-var app 		= express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var XMLHttpRequest  = require("xmlhttprequest").XMLHttpRequest;
+var colors          = require( 'colors' );
+var shell           = require( 'shelljs' );
+var express 	    = require( 'express' );
+var path		    = require( 'path' );
+var mysql      	    = require( 'mysql' );
+var session 	    = require( 'express-session' );
+var bodyParser 	    = require( 'body-parser' );
+var app 		    = express();
+var http            = require('http').Server(app);
+var io              = require('socket.io')(http);
+var ioClient        = require('socket.io-client');
 
+var master = "http://localhost:3000";
+
+var args = process.argv.slice(2);
+var port = args[0];
+var id = args[1];
+var name = args[2];
 
 var map;
 
 var MAX_PLAYERS = 4;
-var BOMB_RATIO = 2;
+var BOMB_RATIO = 5;
 var CODE_SIZE = 6;
 
 var CODE_LEFT = 1;
@@ -19,300 +29,243 @@ var CODE_TOP = 2;
 var CODE_RIGHT = 3;
 var CODE_BOTTOM = 4;
 
+var MESSAGE = 0;
+var INFO = 1;
+var WARNING = 2;
+var ERROR = 3;
+var SUCCESS = 4;
+
+
 var players = [];
 
 var error = {
-	status: "error",
-	code: 0,
-	message: ""
+    status: "error",
+    code: 0,
+    message: ""
 };
 
-/** 
-	MySQL Connect
-*/
+/**
+ MySQL Connect
+ */
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'h4Q{3e;L',
-  database : 'ARDSweeper'
+    host     : 'localhost',
+    user     : 'root',
+    password : 'h4Q{3e;L',
+    database : 'ARDSweeper'
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 
 
 /**
-	Configurations
-*/
+ Configurations
+ */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(session({
-	resave: false,
-	saveUninitialized: false,
-	secret: "this is a secret passphrase"
+    resave: false,
+    saveUninitialized: false,
+    secret: "this is a secret passphrase"
 }));
 
 
-function setup() {	
-	var rows = 10,
-		cols = 10;
-	map = generateMap(rows * cols, rows, cols);
+function setup() {
+    var rows = 10,
+        cols = 10;
+    map = generateMap(rows * cols, rows, cols);
+    log(INFO, "Setting up...");
 }
 
-
-function auth(username, password, fn) {
-	//MySQL Query
-	var query = "SELECT DISTINCT(users.id), users.username, tokens.token FROM users INNER JOIN tokens ON tokens.user_id = users.id WHERE users.username = '" + username + "' AND users.password = '" + password + "' ORDER BY tokens.created_at DESC";
-	connection.query(query, function(err, rows, fields) {
-		if(err)
-			return fn(new Error(err.code));
-		var user = {user: rows[0]};
-		if(!user.user) return fn(new Error("User does not exist"));
-		return fn(null, user);
-	});
-}
-
-function register(u, p, fn) {
-	var query = "INSERT INTO users SET ?";
-	if(!u || !p)
-		return fn(new Error("Les champs n'ont pas été renseignés."));
-
-	connection.query(query, {username: u, password: p}, function(err, rows, fields) {
-		if(err) 
-			return fn(new Error("Erreur lors de l'inscription"));
-		var id = rows.insertId;
-		setToken(id, function(err, token) {
-			if(token)
-				return true;
-			return false;
-		})
-		if(!id) return fn(new Error("User not registered"));
-		return fn(null, id);
-	});
-}
-
-function getUserByID(ID, fn) {
-	connection.query("SELECT DISTINCT(users.id), users.username, tokens.token FROM users INNER JOIN tokens ON tokens.user_id = users.id WHERE users.id = ? ORDER BY tokens.created_at DESC", ID, function(err, rows, fields) {
-		if(err)
-			return fn(new Error(err.code));
-		var user = {user: rows[0]};
-		if(!user.user) return fn(new Error("User not found"));
-		return fn(null, user);
-	});
-}
-
-function setToken(ID, fn) {
-	var t = Math.floor((Math.random() * 99999999999999) + 1000000000000); 
-	connection.query("INSERT INTO tokens SET ?", {user_id: ID, token: t}, function(err, rows, fields) {
-		if(err)
-			return fn(new Error("Token can't be created"));
-		token = rows.insertId;
-		if(!token) return fn(new Error("Token not found"));
-		return fn(null, token);
-	});
-}
-
-function restrict(req, res, next) {
-	if(req.session.user) {
-		next();
-	} else {
-		req.session.error = "Access denied";
-		res.redirect('/user/login');
-	}
+function ping() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", master + "/server/" + id + "/live", false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
 
 function _error(code, message) {
-	error.code = code;
-	error.message = message;
-	log(message);
+    error.code = code;
+    error.message = message;
+    log(message);
 }
 
-function log(message) {
-	var date = new Date();
-	console.log(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " - " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " :: " + message);
-}
+function log(type, message) {
+    type = type || MESSAGE;
+    var date = new Date();
 
+    //Parse date
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if(day < 10) day = "0" + day;
+    if(month < 10) month = "0" + month;
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
+
+    var output = day + "/" + month + "/" + date.getFullYear() + " - " + hours + ":" + minutes  + ":" + seconds + " :: " + message;
+
+    switch (type) {
+        case MESSAGE :
+            console.log(output);
+            break;
+
+        case SUCCESS :
+            console.log(colors.green(output));
+            break;
+
+        case INFO :
+            console.log(colors.cyan(output));
+            break;
+
+        case WARNING :
+            console.log(colors.yellow(output));
+            break;
+
+        case ERROR :
+            console.log(colors.red(output));
+            break;
+    }
+}
 
 
 function generateMap(n, rows, cols) {
-	var map = [];
-	for(i = 0; i < rows; i++) {
-		map[i] = [];
-		for(j = 0; j < cols; j++) {
-			var bomb = Math.floor((Math.random() * BOMB_RATIO) + 1);
-			if(bomb > 1)
-				bomb = 0;
-			map[i][j] = [bomb, 0];
-		}
-	}
-	return map;
+    var map = [];
+    for(i = 0; i < rows; i++) {
+        map[i] = [];
+        for(j = 0; j < cols; j++) {
+            var bomb = Math.floor((Math.random() * BOMB_RATIO) + 1);
+            if(bomb > 1)
+                bomb = 0;
+            map[i][j] = [bomb, -1];
+        }
+    }
+    return map;
 }
 
 function generateCode() {
-	var code = "";
-	for(var i = 0; i < CODE_SIZE; i++)
-		code = code + "" + Math.floor((Math.random() * 4) + 1);
-	return code;
+    var code = "";
+    for(var i = 0; i < CODE_SIZE; i++)
+        code = code + "" + Math.floor((Math.random() * 4) + 1);
+    return code;
 }
 
 
-
 app.get('/', function(req, res) {
-	res.send("Hello world !");
-});
-
-/**
-*	POST: 
-*/
-app.post('/user/login', function(req, res) {
-	var username = req.body.username,
-        password = req.body.password;
-
-    log("Login request");
-
-    auth(username, password, function(err, user) {
-    	if(user) {
-    		req.session.regenerate(function() {
-    			req.session.user = user;
-    		});
-    		log("Login success for User : " + user.user.username);
-    		res.status(200).send(user);
-    	} else {
-			_error(err.code, err.message);
-    		res.status(404).send(error);
-    	}
-    });
-});
-
-app.post('/user/register', function(req, res) {
-	var username = req.body.username,
-        password = req.body.password;
-
-    log("Register request");
-
-   	register(username, password, function(err, ID) {
-   		if(ID) {
-   			getUserByID(ID, function(err2, user) {
-   				if(user) {
-   					res.status(200).send(user);
-    				log("Register success for User : " + user.user.username);
-		    	} else {
-					_error(err2.code, err2.message);
-   					res.status(404).send(error);
-   				}
-   			});
-   		} else {
-			_error(err.code, err.message);
-   			res.status(404).send(error);
-   		}
-   	});
-});
-
-app.get('/user/:id', function(req, res) {
-    log("Get user request");
-	var id = req.params.id;
-	getUserByID(id, function(err, user) {
-		if(user) {
-    		log("Get user success");
-			res.status(200).send(user);
-		} else {
-			_error(err.code, err.message);
-			res.status(404).send(error);
-		}
-	});
+    res.send("Hello world !");
 });
 
 io.on('connection', function(socket) {
-	log("---------------------");
-	log("---------------------");
-	log("[Request] connection");
+    log(MESSAGE, "---------------------");
+    log(MESSAGE, "---------------------");
+    log(INFO, "[Request] connection");
 
-	socket.on('new player', function(username) {
-		
-		if(players.length >= 4) {
-			log("[Emit] serveur full...");
-			socket.emit("server full");
-			socket.emit("disconnect");
-		}
+    socket.on('new player', function(username) {
+        if(players.length >= 4) {
+            log("[Emit] serveur full...");
+            socket.emit("server full");
+            socket.emit("disconnect");
+        }
 
-		socket.username = username;
-		players.push(socket);
-		log("[INTERNAL] user " + username + " added to players list");
-		log("[INTERNAL] users : " + players.length + "/" + MAX_PLAYERS);
-		socket.broadcast.emit("new player");
-	});
+        socket.username = username;
+        players.push(socket);
+        log(SUCCESS, "[INTERNAL] user " + username + " added to players list");
+        log(INFO, "[INTERNAL] users : " + players.length + "/" + MAX_PLAYERS);
+        socket.broadcast.emit("new player");
+    });
 
-	/*
-	socket.on('get players list', function() {
-		log("[Request] get player list");
-		socket.emit('players list', players);
-		log("[Emit] player list");
-	});
-	*/
+    /*
+     socket.on('get players list', function() {
+     log("[Request] get player list");
+     socket.emit('players list', players);
+     log("[Emit] player list");
+     });
+     */
 
-	socket.on('position', function(x, y) {
-		log("[Request] set new position [" + x + "," + y + "]");
-		if(map[x][y][0] == 1) {
-			log("--- BOMB ---");
-			var code = generateCode();
-			log("Code : " + code);
-			io.sockets.emit("bomb");
-		} else {
-			io.broadcast.emit("position", x, y);
-		}
-	});
+    socket.on('position', function(x, y) {
+        x = parseInt(x);
+        y = parseInt(y);
 
-	socket.on('code', function(token, code) {
-		log("User Code");
-	});
+        log(INFO, "[Request] set new position [" + x + "," + y + "]");
+        map[x][y][1] = -2;
+        if(map[x][y][0] == 1) {
+            map[x][y][1] = -3;
+            log(WARNING, "--- BOMB ---");
+            var code = generateCode();
+            log(WARNING, "Code : " + code);
+            io.sockets.emit("bomb");
+        }
+        var state = map[x][y][1];
+        io.sockets.emit("position", x, y, state);
+    });
 
-	socket.on('get full map', function(token) {
-		log("[Request] full map");
-		console.log(JSON.stringify(map));
-		socket.emit('full map', map);
-		log("[Emit] full map");
-	});
+    socket.on('code', function(token, code) {
+        log(INFO, "User Code");
+    });
 
-	socket.on('full map client', function() {
-		log("[Request] map fully loaded by the client");
-		socket.emit('start game');
-		log("[Emit] start the game");
-	});
+    socket.on('get full map', function(token) {
+        log(INFO, "[Request] full map");
+        socket.emit('full map', map);
+        log(INFO, "[Emit] full map");
+    });
 
-	socket.on('game loaded', function() {
-		log("[Request] game successfully loaded");
-	});
+    socket.on('full map client', function() {
+        log(INFO, "[Request] map fully loaded by the client");
+        socket.emit('start game');
+        log(INFO, "[Emit] start the game");
+    });
 
-	socket.on('bomb desengaged', function() {
-		log("BOMB DESENGAGED");
-		socket.broadcast.emit("bomb explode");
-	});	
+    socket.on('game loaded', function() {
+        log(SUCCESS, "[Request] game successfully loaded");
+    });
 
-	socket.on('disconnect', function() {
-		log('[Request] client disconnect');
-		var i = players.indexOf(socket);
-		players.splice(i, 1);
-	});
+    socket.on('bomb desengaged', function() {
+        log(SUCCESS, "BOMB DESARMED");
+        socket.broadcast.emit("bomb explode");
+    });
 
-	socket.on('error', function (err) { console.error(err.stack); });
+    socket.on('disconnect', function() {
+        log(WARNING, '[Request] client disconnect');
+        var i = players.indexOf(socket);
+        players.splice(i, 1);
+    });
+
+    socket.on('pong', function() {
+       socket.emit('ping');
+    });
+
+    socket.on('error', function (err) { console.error(err.stack); });
 
 });
 
+app.get('/players', function(req, res) {
+    //Liste des serveurs
+    log(INFO, "[SERVER][GET] get players");
+    res.status(200).send(players.length);
+});
 
 
-var server = http.listen( 3000, function() {
+var server = http.listen( port, function() {
 
-	var host = server.address().address;
-	var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-	setup();
+    shell.exec("clear");
 
-	console.log("Server running at http://%s:%s", host, port);
+    setup();
+    ping();
+    setInterval(ping, 5000);
+
+    log(INFO, "game server (" + id + ") start running at port " + port);
 
 });
